7a95fb4f71a088ac09ace631c1691d0d49fcbc84
bhupendrasinghacme <bhupendrasingh@acmeintech.in>
2022-10-14 15:36:52 +0530

    bhupendra 14 oct

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-app-base',
+  appId: 'io.hot.tawaa',
+  appName: 'Hot Tawaa',
   webDir: 'www',
   bundledWebRuntime: false
 };
